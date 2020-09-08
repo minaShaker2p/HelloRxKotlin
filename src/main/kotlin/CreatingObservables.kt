ef9e6661a782c5cd36965bfed969fd77a9d2bc02
Mina Rezkalla <minashakeralfy@gmail.com>
2020-09-08 16:50:28 +0200

    add sealed class for Exceptions

--- src/main/kotlin/CreatingObservables.kt
+++ src/main/kotlin/CreatingObservables.kt
@@ -2,23 +2,29 @@ import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 
 
-fun createObservable()
+sealed class Droid : Throwable() {
+    class OU812 : Droid()
+}
+
+sealed class FileReadError : Throwable()
 {
+    class FileNotFound : FileReadError()
+}
+
+
+fun createObservable() {
     // observable of string
-   val mostPopular : Observable<String> = Observable.just(episodeI)
+    val mostPopular: Observable<String> = Observable.just(episodeI)
 
     // observable of strings
-    val originalTrilogy = Observable.just(episodeIV,episodeV,episodeIII)
+    val originalTrilogy = Observable.just(episodeIV, episodeV, episodeIII)
 
     // Observable of list
-    val prequelTrilogy = Observable.just(listOf(episodeI,episodeII,episodeIII))
+    val prequelTrilogy = Observable.just(listOf(episodeI, episodeII, episodeIII))
 
     // Observable from ITERABLE
-    val sequelTrilogy = Observable.fromIterable(listOf(episodeVI,episodeVII,episodeIX))
+    val sequelTrilogy = Observable.fromIterable(listOf(episodeVI, episodeVII, episodeIX))
 
     //  Observable from list
-    val stories = listOf<String>(solo,rogueOne).toObservable()
-
-
-
+    val stories = listOf<String>(solo, rogueOne).toObservable()
 }
